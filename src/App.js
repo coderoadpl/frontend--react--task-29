3ae2848d5d826fd9e7347c0c0ff8b7cd0f131bd5
Mateusz Choma <kontakt@coderoad.pl>
2021-06-14 21:04:50 +0200

    `prop-types` string validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Greeter = (props) => {
+  const { name } = props
+
+  return (
+    <h1>
+      Hello {name}!
+    </h1>
+  )
+}
+
+Greeter.propTypes = {
+  name: PropTypes.string
+}
 
 export const App = () => {
   return (
     <div>
-      CodeRoad APP
+      <Greeter
+        name={'Mateusz'}
+      />
+      <Greeter
+        name={123}
+      />
+      <Greeter />
     </div>
   )
 }
